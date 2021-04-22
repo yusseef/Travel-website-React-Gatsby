d182561fd9ae43e32f6199252fb42f1bcf19e409
yusseef <yussef.raouf11@gmail.com>
2021-04-22 12:53:19 +0200

    add images

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./HeroSection.css"
+
+function HeroSection() {
+    return (
+        <div className= "hero-container">
+            <h1>Travel more</h1>
+            <p>Plan your next trip today</p>
+            <div classname= "hero-btns">
+                <button>Get started</button>
+            </div>
+
+        </div>
+    )
+}   
+
+export default HeroSection
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import * as React from "react"
+import HeroSection from "../components/HeroSection"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Home page</h1>
+    
+    <HeroSection />
     
     
   </Layout>
